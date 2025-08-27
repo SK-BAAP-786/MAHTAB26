05b2d8bb96bcbb489fc4912ad83867a27e0e1ce1
SK BAAP G <111557947+SK-BAAP-786@users.noreply.github.com>
2025-08-27 17:32:39 +0530

    Update Arun1.js

--- Priyansh/commands/Arun1.js
+++ Priyansh/commands/Arun1.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("Mahtab") || 
 react.includes("mahtab")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n 𝗠𝗮𝗵𝘁𝗮𝗯✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦  ✦ \n𝗝𝗼𝗶𝗻 𝗠𝘆 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗜𝗱 \n 𝗠𝗮𝗵𝘁𝗮𝗯 𝗔𝗵𝗺𝗮𝗱 😋https://www.facebook.com/mahtab.ahmad.985178`",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n 𝗠𝗮𝗵𝘁𝗮𝗯✦😈:\n\n✦  ✦ \n𝗝𝗼𝗶𝗻 𝗠𝘆 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗜𝗱 \n 𝗠𝗮𝗵𝘁𝗮𝗯 𝗔𝗵𝗺𝗮𝗱 😋https://www.facebook.com/mahtab.ahmad.985178`",
 				attachment: fs.createReadStream(__dirname + `/noprefix/Screenshot_20250814_124812.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
